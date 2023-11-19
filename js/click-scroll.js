0c09394966301a44b070b7159b334751fdc63932
beanusaforum <beanusafest@gmail.com>
2023-11-19 21:07:59 +0700

    Ganti Nadiem ke Dito

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -7,17 +7,14 @@ $(document).scroll(function () {
         var sectionId = 'section_' + value;
         var sectionElement = $('#' + sectionId);
 
-        // Pemeriksaan apakah elemen dengan id yang diharapkan ada
         if (sectionElement.length > 0) {
             var offsetSection = sectionElement.offset().top - 84;
             var docScroll = $(document).scrollTop();
             var docScroll1 = docScroll + 1;
 
             if (docScroll1 >= offsetSection) {
-                $('.navbar-nav .nav-item .nav-link').removeClass('active');
-                $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
-                $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-                $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+                $('.navbar-nav .nav-item .nav-link').removeClass('active inactive');
+                $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active').removeClass('inactive');
             }
         } else {
             console.error('Elemen dengan id ' + sectionId + ' tidak ditemukan.');
@@ -26,16 +23,24 @@ $(document).scroll(function () {
 });
 
 $('.click-scroll').click(function (e) {
-    var index = $(this).index('.click-scroll');
-    var offsetClick = $('#' + 'section_' + sectionArray[index]).offset().top - 84;
     e.preventDefault();
-    $('html, body').animate({
-        'scrollTop': offsetClick
-    }, 300);
+    var index = $(this).index('.click-scroll');
+    
+    // Cek apakah ini link ke formulir pendaftaran
+    if (sectionArray[index] === 7) {
+        // Gantilah URL_FORMULIR dengan URL formulir Google Anda
+        var urlFormulir = "http://bit.ly/beanusafest";
+        window.open(urlFormulir, "_blank");
+    } else {
+        var sectionId = 'section_' + sectionArray[index];
+        var offsetClick = $('#' + sectionId).offset().top - 84;
+        $('html, body').animate({
+            'scrollTop': offsetClick
+        }, 300);
+    }
 });
 
 $(document).ready(function () {
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
-    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
-    $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
+    $('.navbar-nav .nav-item .nav-link').addClass('inactive');
+    $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active').removeClass('inactive');
 });
